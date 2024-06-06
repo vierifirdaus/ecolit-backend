7acec0c8865916a4ad8908c71a4bb418230406e8
Vieri Fajar Firdaus <vieryfirdaus2527@gmail.com>
2024-06-06 20:41:19 +0700

    fix

--- src/utils/qurbanSetup.ts
+++ src/utils/qurbanSetup.ts
@@ -10,7 +10,6 @@ const authDrive = new GoogleAuth({
     keyFile: './qurbanservice.json',
     scopes: 'https://www.googleapis.com/auth/drive',
 });
-
 export const sheetsService = google.sheets({version: 'v4', auth});
 
 export const driveServices = google.drive({version: 'v3', auth:authDrive});
